40ec3d248321a684fe48c5c65c0f0521b7667ffc
Thuong <truonghoaithuong2000@gmail.com>
2018-06-03 20:43:44 +0700

    fix bug

--- app/jobs/get_data.rb
+++ app/jobs/get_data.rb
@@ -10,12 +10,12 @@ class GetData
     stations.each do |station|
       res = HTTParty.get(station.api)
       lastOnline = Time.now - res['HappenedTime'].to_time
-      if lashOnline > 3600
+      if lastOnline > 3600
         puts "#{res['StationName']} is down"
         down_stations.push(res['StationName'])
       end
     end
-    down_stations || UserMailer.stations_notify_email(down_stations).deliver_now
+    down_stations && UserMailer.stations_notify_email(down_stations).deliver_now
   end
 
 end

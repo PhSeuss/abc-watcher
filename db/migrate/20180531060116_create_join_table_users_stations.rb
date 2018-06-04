7d362f72a6ae19925ffacb7c0736d0d9148059d1
Thuong <truonghoaithuong2000@gmail.com>
2018-06-04 10:00:59 +0700

    roll back false association

--- db/migrate/20180531060116_create_join_table_users_stations.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableUsersStations < ActiveRecord::Migration[5.1]
-  def change
-    create_join_table :users, :stations do |t|
-      t.index [:user_id, :station_id]
-      t.index [:station_id, :user_id]
-    end
-  end
-end
